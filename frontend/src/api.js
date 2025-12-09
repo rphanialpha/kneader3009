afd83179dc111505337fc671414683ce303a6921
subhalakshmi-3009 <subha@alphaworkz.com>
2025-12-09 16:25:37 +0000

    9/12/2025

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://shera-undefensible-pseudoindependently.ngrok-free.dev';
+const API_BASE_URL =  https://shera-undefensible-pseudoindependently.ngrok-free.dev;
 
 
 const api = axios.create({
